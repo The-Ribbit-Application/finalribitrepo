30aa429c6e4f1309b0e9b9e98b43b173ab657f22
Owen Poling <owenpoling21@gmail.com>
2023-04-21 15:43:31 -0400

    Big commit of the functioning features

---
+++ lib/Pages/login_page.dart
@@ -0,0 +1,456 @@
+import 'package:flutter/material.dart';
+import 'package:finalribitrepo/UI Components/SquareTile.dart';
+import 'package:finalribitrepo/UI Components/todobutton.dart';
+
+import '../UI Components/Textfield.dart';
+import '../UI Components/button.dart';
+import '../UI Components/timerbutton.dart';
+import '../UI Components/goback.dart';
+
+// Owen's imports for todo
+import '../model/todo.dart';
+import '../UI Components/todocolors.dart';
+import '../widgets/todo_item.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  //text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+//SignUserIn method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFCFE795),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+              //logo goes here
+              Image.asset("lib/Images/img.png", height: 300),
+              const Text(
+                'Great Grades are a hop away!',
+                style: TextStyle(
+                    color: Color(0xFF447604),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+
+              const SizedBox(height: 50),
+              //Welcome text
+              const Text(
+                'Log-In',
+                style: TextStyle(
+                    color: Color(0xFF447604),
+                    fontSize: 64,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 25),
+              //username text-field
+              Textfield(
+                controller: usernameController,
+                hintText: "Username",
+                obscureText: false,
+              ),
+              const SizedBox(height: 25),
+              //password text-field
+              Textfield(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
+              const SizedBox(height: 25),
+
+              //forgot password link
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Color(0xFF447604)),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25),
+              button(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const buttonpage()),
+                  );
+                },
+              ),
+
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Divider(
+                      thickness: 1.0,
+                      color: Colors.grey[400],
+                    )),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        "Or continue with",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    Expanded(
+                        child: Divider(
+                      thickness: 1.0,
+                      color: Colors.grey[400],
+                    ))
+                  ],
+                ),
+              ),
+              //google sign in + apple sign in
+              const SizedBox(height: 50),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  //google button
+                  SquareTile(
+                      imagePath: "lib/Images/google logo.png", imageHeight: 60),
+                  SizedBox(width: 50),
+
+                  //apple button
+                  SquareTile(
+                      imagePath: "lib/Images/apple logo.png", imageHeight: 60),
+                ],
+              ),
+
+              //Don't have an account? Register Now
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text("Don't have an account?"),
+                  SizedBox(width: 4),
+                  Text(
+                    "Sign up now!",
+                    style: TextStyle(
+                        color: Color(0xFF447604), fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class buttonpage extends StatelessWidget {
+  const buttonpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFCFE795),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Great Grades are a hop away!',
+                style: TextStyle(
+                    color: Color(0xFF447604),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 200),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              ),
+              const SizedBox(height: 25),
+              timerbutton(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const timerpage()),
+                  );
+                },
+              ),
+              const SizedBox(height: 25),
+              todobutton(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                },
+              ),
+              const SizedBox(height: 200),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              ),
+              const SizedBox(height: 25),
+              goback(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class timerpage extends StatelessWidget {
+  const timerpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFCFE795),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 25),
+              goback(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Owen's feature
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final todosList = ToDo.todoList();
+  List<ToDo> _foundToDo = [];
+  final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundToDo = todosList;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 15,
+            ),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'Task List',
+                          style: TextStyle(
+                            fontSize: 45,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      for (ToDo todoo in _foundToDo.reversed)
+                        ToDoItem(
+                          todo: todoo,
+                          onToDoChanged: _handleToDoChange,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: InputDecoration(
+                        hintText: 'Add a new task item',
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                  ),
+                  onPressed: () {
+                    _addToDoItem(_todoController.text);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: tdBlue,
+                    minimumSize: Size(60, 60),
+                    elevation: 10,
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleToDoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void _deleteToDoItem(String id) {
+    setState(() {
+      todosList.removeWhere((item) => item.id == id);
+    });
+  }
+
+  void _addToDoItem(String toDo) {
+    setState(() {
+      todosList.add(ToDo(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        todoText: toDo,
+      ));
+    });
+    _todoController.clear();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<ToDo> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = todosList;
+    } else {
+      results = todosList
+          .where((item) => item.todoText!
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      _foundToDo = results;
+    });
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        onChanged: (value) => _runFilter(value),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.black,
+            //color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Icon(
+          Icons.menu,
+          color: Colors.black,
+          size: 30,
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/images/avatar.jpeg'),
+          ),
+        ),
+      ]),
+    );
+  }
+}
