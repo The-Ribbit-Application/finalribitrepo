9b3fdab561c6de44a9574ef6146f17855631604b
Osaze Imohe <77579038+osazeimohe@users.noreply.github.com>
2023-04-21 17:41:21 -0400

    added timerpage, and buttonpage to separate files

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -4,7 +4,6 @@ import 'package:finalribitrepo/UI Components/SquareTile.dart';
 
 import '../UI Components/Textfield.dart';
 import '../UI Components/button.dart';
-import 'buttonpage.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
@@ -26,121 +25,123 @@ class LoginPage extends StatelessWidget {
       backgroundColor: const Color(0xFFCFE795),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-              //logo goes here
-              Image.asset("lib/Images/img.png", height: 300),
-              const Text(
-                'Great Grades are a hop away!',
-                style: TextStyle(
-                    color: Color(0xFF447604),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-
-              const SizedBox(height: 50),
-              //Welcome text
-              const Text(
-                'Log-In',
-                style: TextStyle(
-                    color: Color(0xFF447604),
-                    fontSize: 64,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 25),
-              //username text-field
-              Textfield(
-                controller: EmailController,
-                hintText: "Username",
-                obscureText: false,
-              ),
-              const SizedBox(height: 25),
-              //password text-field
-              Textfield(
-                controller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-              ),
-              const SizedBox(height: 25),
-
-              //forgot password link
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Color(0xFF447604)),
-                    ),
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                //logo goes here
+                Image.asset("lib/Images/img.png", height: 300),
+                const Text(
+                  'Great Grades are a hop away!',
+                  style: TextStyle(
+                      color: Color(0xFF447604),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-              const SizedBox(height: 25),
-              button(
-                onTap: signUserIn,
-                buttonText: 'Sign In',
-              ),
 
-              const SizedBox(height: 50),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Divider(
-                      thickness: 1.0,
-                      color: Colors.grey[400],
-                    )),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        "Or continue with",
-                        style: TextStyle(color: Colors.grey),
+                const SizedBox(height: 50),
+                //Welcome text
+                const Text(
+                  'Log-In',
+                  style: TextStyle(
+                      color: Color(0xFF447604),
+                      fontSize: 64,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 25),
+                //username text-field
+                Textfield(
+                  controller: EmailController,
+                  hintText: "Username",
+                  obscureText: false,
+                ),
+                const SizedBox(height: 25),
+                //password text-field
+                Textfield(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+
+                //forgot password link
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Color(0xFF447604)),
                       ),
-                    ),
-                    Expanded(
-                        child: Divider(
-                      thickness: 1.0,
-                      color: Colors.grey[400],
-                    ))
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 25),
+                button(
+                  onTap: signUserIn,
+                  buttonText: 'Sign In',
+                ),
+
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Divider(
+                        thickness: 1.0,
+                        color: Colors.grey[400],
+                      )),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          "Or continue with",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                      Expanded(
+                          child: Divider(
+                        thickness: 1.0,
+                        color: Colors.grey[400],
+                      ))
+                    ],
+                  ),
+                ),
+                //google sign in + apple sign in
+                const SizedBox(height: 50),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    //google button
+                    SquareTile(
+                        imagePath: "lib/Images/google logo.png", imageHeight: 60),
+                    SizedBox(width: 50),
+
+                    //apple button
+                    SquareTile(
+                        imagePath: "lib/Images/apple logo.png", imageHeight: 60),
                   ],
                 ),
-              ),
-              //google sign in + apple sign in
-              const SizedBox(height: 50),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  //google button
-                  SquareTile(
-                      imagePath: "lib/Images/google logo.png", imageHeight: 60),
-                  SizedBox(width: 50),
-
-                  //apple button
-                  SquareTile(
-                      imagePath: "lib/Images/apple logo.png", imageHeight: 60),
-                ],
-              ),
-
-              //Don't have an account? Register Now
-              const SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text("Don't have an account?"),
-                  SizedBox(width: 4),
-                  Text(
-                    "Sign up now!",
-                    style: TextStyle(
-                        color: Color(0xFF447604), fontWeight: FontWeight.bold),
-                  ),
-                ],
-              )
-            ],
+                //Don't have an account? Register Now
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("Don't have an account?"),
+                    SizedBox(width: 4),
+                    Text(
+                      "Sign up now!",
+                      style: TextStyle(
+                          color: Color(0xFF447604), fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
