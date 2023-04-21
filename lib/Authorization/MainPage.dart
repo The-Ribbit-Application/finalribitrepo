f8bfdb1ed1aea6d15537fd3101263d1501fcbdc5
Osaze Imohe <77579038+osazeimohe@users.noreply.github.com>
2023-04-21 17:43:21 -0400

    added timerpage, and buttonpage to separate files

---
+++ lib/Authorization/MainPage.dart
@@ -0,0 +1,30 @@
+//page for seeing if we're logged in or not
+import 'package:finalribitrepo/Pages/buttonpage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../Pages/home.dart';
+import '../Pages/login_page.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            return buttonpage();
+          }else{
+            return LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+
